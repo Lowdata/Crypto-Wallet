ea7c10aab10e879a1ed1ff9392fe076f7af24969
ayushPahuja <ayush.pahuja090@gmail.com>
2024-01-05 13:42:16 +0530

    babel-congig updated

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,22 @@
 module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-        ["rewrite-require", { aliases: {
-          "stream": "stream-browserify"
-        }}]
-      ]
+    api.cache(true);
+    return {
+      presets: [
+        ['babel-preset-expo'],
+        [
+          '@babel/preset-env',
+          {
+            useBuiltIns: 'usage',
+            corejs: 3,
+            targets: 'defaults',
+          },
+        ],
+      ],
+      plugins: [
+        ['rewrite-require', { aliases: { stream: 'stream-browserify' } }],
+        '@babel/plugin-transform-private-methods',
+        '@babel/plugin-proposal-class-properties',
+      ],
+    };
   };
-};
+  
